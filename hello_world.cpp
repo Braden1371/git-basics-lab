46a4bd5fc25626954aaae5ace6dc776baf3911ad
Braden Harris <brharr@cs.csis.work>
2023-09-03 20:19:13 -0500

    The arrogance of the jedi

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,4 +3,11 @@
 int main(){
   cout << "Hello World" << endl;
 
+  //Hellow world is too easy, time to add more!
+  //Time to add important info
+  string userInput;
+  cout << "Did you ever hear the tragedy of Darth Plaguies the wise? " << endl;
+  cin >> userInput;
+  cout << "I thought not. It's not a story the Jedi would tell you." << endl;
+
 }
