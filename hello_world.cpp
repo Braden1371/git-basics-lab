aea27d2a06b8b097c162ca83c3afe88505f07318
Braden Harris <brharr@cs.csis.work>
2023-09-03 20:31:09 -0500

    Added functionality to question

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,8 +6,14 @@ int main(){
   //Hellow world is too easy, time to add more!
   //Time to add important info
   string userInput;
-  cout << "Did you ever hear the tragedy of Darth Plaguies the wise? " << endl;
-  cin >> userInput;
+  while(userInput != "no" || userInput != "No"){
+ 	 cout << "Did you ever hear the tragedy of Darth Plaguies the wise? " << endl;
+ 	 cin >> userInput;
+
+	 if (userInput != "no" || userInput != "No"){
+		 cout << "Search your feelings, and try again" << endl;
+  }
+
   cout << "I thought not. It's not a story the Jedi would tell you." << endl;
 
 }
