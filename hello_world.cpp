8ad5bdfbd2401f4cd976f5ba18e7070203dc93ed
Braden Harris <brharr@cs.csis.work>
2023-09-05 18:43:48 -0500

    Added story

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,19 +1,30 @@
 // First github program
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main(){
-  cout << "Hello World" << endl;
 
   //Hellow world is too easy, time to add more!
   //Time to add important info
   string userInput;
-  while(userInput != "no" || userInput != "No"){
+  bool correct = false;
+  while(correct == false){
  	 cout << "Did you ever hear the tragedy of Darth Plaguies the wise? " << endl;
  	 cin >> userInput;
 
-	 if (userInput != "no" || userInput != "No"){
+	 cout << userInput << endl;
+
+	 if (userInput != "no" && userInput != "No"){
 		 cout << "Search your feelings, and try again" << endl;
+	 }
+
+	 else
+		 correct = true;
   }
 
-  cout << "I thought not. It's not a story the Jedi would tell you." << endl;
-
+  cout << "I thought not. It's not a story the Jedi would tell you." << endl
+  	<< "It's a sith legend. Darth Plagueis was a Dark Lord of the Sith, so " << endl 
+  	<< "powerfull and wise he could use the Force to influence midichlorians " << endl
+  	<< "to create life." << endl;
 }
